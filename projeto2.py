4c0840c9877f18b6a88e9b111e85b4c5bb0e7312
Alexandre henrich <alexandrehenrich45@gmail.com>
2025-05-26 21:26:17 -0300

    Add files via upload

---
+++ projeto2.py
@@ -0,0 +1,151 @@
+# nome_var="Alexandre"
+# idade_var=18
+# nota_var=9.8
+# print(f"ola meu nome e {nome_var} tenho {idade_var} anos de idade e fui aluno destaque com a nota de {nota_var}")
+
+
+
+
+
+# x = 50
+# x += 90
+# print(x)
+
+# x = 50
+# x //=3
+# print(x)
+
+# x = 99
+# x /= 5
+# print(x)
+
+# x= 50
+# x -= 60
+# print(x)
+
+
+
+
+
+
+# if nota_var >= 6:
+#     print("aluno aprovado")
+# else:
+#     print("aluno reprovado")
+
+
+
+
+
+
+# for i in range(10):
+#     i +=1
+#     print(i)
+
+
+
+
+
+# def somar(a, b):
+#     return a+b
+# resultado = somar(33,22)
+# print(resultado)
+
+
+
+
+
+# nome_var=""
+# idade_var=''
+# nome_var=input("qual seu nome?")
+# idade_var=input("qual sua idade?")
+# print(f"meu nome é {nome_var} tenho {idade_var} anos de idade")
+
+
+
+
+# lista=["morango","ameixa","melancia","manga","jabuticaba"]
+# print(f"{lista[1]}, {lista[-1]}")
+
+
+
+
+# matriz=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+#         ]
+# print=(f"{matriz[0][0]},{matriz[1][1]},{matriz[2][2]}")
+
+
+
+
+# try:
+#     x= int(input("Digite o numerador:"))
+#     y= int(input("Digite o denominador:"))
+#     print(x/y)
+# except ValueError:
+#  print("Digite apenas numeros")
+
+
+
+# texto="Olá alunos"
+# print(texto.upper())
+# print(texto.lower())
+# print(texto.strip())
+ 
+ 
+
+# while True:
+#     i=int(input("Encira um numero maior que zero"))
+#     if i > 1:   
+#         continue
+#     else:
+#         break
+
+
+# for a in range(6):
+#     for b in range(11):
+#         print(a*b)
+
+
+
+# for a in range(0,31,3):
+#     print(a)
+
+
+# tupla=("Segunda","Terça","Quarta")
+# print(f"{tupla[1]}")
+
+
+# idade=input("Qual sua idade?")
+# print(f"minha idade é {idade}")
+# "Maior de idade" if idade == 18 else "Menor de idade"
+
+
+# def saudacao(nome,curso):
+#     print(f"ola meu nome e {nome} faço curso de {curso}")
+
+# saudacao("Alexandre","Desenvolvmento de sistema")
+
+
+
+# def saudacao(nome,curso):
+#      return print(f"ola meu nome e {nome} faço curso de {curso}")
+
+# saudacao("Alexandre","Desenvolvmento de sistema")
+
+
+
+# alunos={}
+# while True:
+#    aluno=[]
+#    alunos["Nome"]= input("Qual é seu nome?")
+#    alunos["idade"]= int(input("Qual sua idada?"))
+#    alunos["Curso"]= input("Deseja fazer qual curso?")
+#    aluno.append(alunos)
+#    continuar= input("Deseja adicionar outro aluno? (s/n)").lower()
+#    if continuar == "n":
+#     break
+#    else:
+#      print(aluno)
